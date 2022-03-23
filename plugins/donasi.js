d0021a102ced24102374c72030c1828ae168f02f
BunyanXd <101381544+BunyanXd@users.noreply.github.com>
2022-03-23 17:49:07 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -5,8 +5,8 @@ let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fet
 Hai 👋
 Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
 ┌〔 Donasi • Emoney 〕
-├ PULSA : 083830815715
-├ DANA : 082124245042
+├ PULSA : 083863605242
+├ GOPAY : 089502435112
 └────
 `.trim(), 'Donasi bg Biar Bot Nya on 24Jam', 'SEWA', '.sewa')
 handler.help = ['donasi']
